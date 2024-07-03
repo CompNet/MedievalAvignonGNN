d7597f6e3238467cda864b6c2ce1f58fe48d9a3f
Victor ONIC <victor.onic@outlook.fr>
2024-07-03 14:54:10 +0200

    Remove attributes from some nodes & Update colors

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MedievalAvignonGNN
 Vertex position estimation in spatial graphs
+
+
+## Create a graph
+The ```graph_generator``` script can be run by itself. It is also used by the other scripts as an import.
+
+```
+usage: graph_generator [-h] [-n NODES] [-d DENSITY] [-u UNKNOWN] [-i] [-s]
+
+options:
+  -h, --help            show this help message and exit
+  -n NODES, --nodes NODES
+                        number of nodes (default=10)
+  -d DENSITY, --density DENSITY
+                        density of the graph (default=0.5)
+  -u UNKNOWN, --unknown UNKNOWN
+                        proportion of nodes without position (default=0.5)
+  -i, --image           show graph
+  -s, --save            save graph object to disk
+```
--- src/graph_generator.py
+++ src/graph_generator.py
@@ -6,35 +6,40 @@ import matplotlib.pyplot as plt
 
 
 
-def gen(nb_nodes: int, density: float) -> nx.DiGraph:
+def gen(nb_nodes: int, density: float, unknown: float) -> nx.DiGraph:
     """
         nb_nodes: Nombre de noeuds du graphe.
         density: Proportion de liens présents par rapport à un graphe complet.
-        attr_min: Valeur minimale de la position (axe X & axe Y).
-        attr_max: Même chose pour la valeur maximale.
+        unknown: Proportion de noeuds sans position.
         
         Retourne une graphe orienté dont les noeuds sont les entiers de 0 à n.
         Les noeuds ont une liste de taille 2 comme attributs de position.
         Les arcs ont une chaîne de caractères comme attribut.
     """
-    first_n_letters = list(range(nb_nodes))
-    G = nx.complete_graph(first_n_letters, nx.DiGraph())
+    G = nx.complete_graph(range(nb_nodes), nx.DiGraph())
 
-    # Dégradation: réduction de densité
+    # Réduction de densité: supprimer certains liens.
     tbr = random.sample(
         tuple(G.edges()), 
         int(nx.number_of_edges(G) * (1 - density))
     )
     G.remove_edges_from(tbr)
 
-    # Attributs
-    for node in G.nodes():
-        G.nodes[node]['pos'] = np.random.uniform(0.0, 1.0, 2).tolist()
+    # Attributs des noeuds.
+    m = np.random.choice([0, 1], size=nb_nodes, p=[unknown, 1-unknown])
+    for i, node in enumerate(G.nodes()):
+        G.nodes[node]['pos'] = np.random.uniform(0.0, 1.0, 2).tolist() if m[i] else list()
 
+    # Attributs des liens.
     for edge in G.edges():
-        dir = ''
+        dir = 'N/A'
         src, dest = edge
         p1, p2 = G.nodes[src]['pos'], G.nodes[dest]['pos']
+
+        if not p1 or not p2:
+            G.edges[edge]['dir'] = dir
+            continue
+
         dx = p1[0] - p2[0]
         dy = p1[1] - p2[1]
 
@@ -81,11 +86,13 @@ def save(G: nx.Graph, filename: str) -> None:
 def display(G: nx.Graph) -> None:
     """
         Afficher le graphe avec pyplot.
+        Les noeuds apparaissent bleus s'ils ont leur attribut, ruoge sinon.
         Les arcs sont colorés selon leurs attributs:
             - Nord : vert
             - Sud : violet
             - Ouest: bleu
             - Est: rouge
+            - sans direction: orange
     """
     def get_color(dir: str) -> str:
         if dir == 'N':
@@ -94,14 +101,18 @@ def display(G: nx.Graph) -> None:
             return 'purple'
         elif dir == 'W':
             return 'blue'
-        else:
+        elif dir == 'E':
             return 'red'
-
-    layout = {node: tuple(G.nodes[node]['pos']) for node in G.nodes}
+        else:
+            return 'orange'
 
     edge_colors = [get_color(v) for k, v in nx.get_edge_attributes(G, 'dir').items()]
+    node_colors = ['red' if not v else 'blue' for k, v in nx.get_node_attributes(G, 'pos').items()]
 
-    nx.draw(G, layout, node_size=50, edge_color=edge_colors, arrowsize=10)
+    # Disposition des noeuds sur l'image
+    layout = nx.kamada_kawai_layout(G)
+
+    nx.draw(G, layout, node_size=50, arrowsize=10, node_color=node_colors, edge_color=edge_colors)
 
     plt.show()
 
@@ -111,13 +122,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser('graph_generator')
 
     parser.add_argument('-n', '--nodes', help='number of nodes (default=10)', type=int, default=10)
-    parser.add_argument('-d', '--density', help='density of the graph (default=0.15)', type=float, default=0.15)
+    parser.add_argument('-d', '--density', help='density of the graph (default=0.5)', type=float, default=0.5)
+    parser.add_argument('-u', '--unknown', help='proportion of nodes without position (default=0.5)', type=float, default=0.5)
     parser.add_argument('-i', '--image', help='show graph', action='store_true')
-    parser.add_argument('-s', '--save', help='save graph to disk', action='store_true')
+    parser.add_argument('-s', '--save', help='save graph object to disk', action='store_true')
 
     p = parser.parse_args()
 
-    G = gen(p.nodes, p.density)
+    G = gen(p.nodes, p.density, p.unknown)
     
     if p.save:
         save(G, f'G_n{p.nodes}_d{p.density}_{datetime.now().strftime("%d%f")}.json')
